157e736cb10d55c3c3c680e38f429bad01f2e7af
Sanketpatil012 <122162948+Sanketpatil012@users.noreply.github.com>
2025-03-14 16:05:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+
+  function handleEvent(){
+       const colorSelect = document.getElementById("colorSelect");
+
+  // Get the selected option
+  const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+
+  // Remove the selected option if one is chosen
+  if (selectedOption) {
+    colorSelect.remove(colorSelect.selectedIndex);
+
+	  
+  }
+
+  }
+
